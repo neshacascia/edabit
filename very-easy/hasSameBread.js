7cc7717da8b88c00ae95b908704ddc09c743f770
Nesha Mervin <nesha.mervin@gmail.com>
2022-08-03 03:54:09 -0400

    added solution

---
+++ very-easy/hasSameBread.js
@@ -0,0 +1,7 @@
+// Similar Bread:
+// Given two arrays, which represent two sandwiches, return whether both sandwiches use the same
+// type of bread. The bread will always be found at the start and end of the array.
+
+function hasSameBread(arr1, arr2) {
+  return arr1[0] === arr2[0] && arr1[2] === arr2[2] ? true : false;
+}

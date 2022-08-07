0f427160da6edd98013f927ead0cf6b3903239a3
Nesha Mervin <nesha.mervin@gmail.com>
2022-08-07 04:07:22 -0400

    added solution

---
+++ very-easy/toNumberArray.js
@@ -0,0 +1,7 @@
+// Array of Strings to Array of Numbers:
+// Create a function that takes as a parameter an array of "stringified" numbers
+// and returns an array of numbers.
+
+function toNumberArray(arr) {
+  return arr.map(Number);
+}

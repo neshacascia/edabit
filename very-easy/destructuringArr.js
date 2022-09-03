cae60f1a9d9f15a5feb5461521e28e295d024007
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-02 21:07:02 -0400

    added solution

---
+++ very-easy/destructuringArr.js
@@ -0,0 +1,5 @@
+// ES6: Destructuring Arrays IV:
+// There is an easy way to assign to array values to the nth index by using rest parameter syntax.
+// But how could I make tail = [2, 3, 4] instead of tail = 2?
+
+const str = '[head, ...tail] = [1, 2, 3, 4]';

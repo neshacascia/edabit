76688598316f1094baa174c8991816ce8da16d57
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-06 01:06:43 -0400

    added solution

---
+++ very-easy/getMultipliedArr.js
@@ -0,0 +1,6 @@
+// Multiply Every Array Item by Two:
+// Create a function that takes an array with numbers and return an array with the elements multiplied by two.
+
+function getMultipliedArr(arr) {
+  return arr.map(num => num * 2);
+}

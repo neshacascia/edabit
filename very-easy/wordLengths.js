63ab6ba8335791844e5f618f62084b03bf2ba4db
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-08 01:09:07 -0400

    added solution

---
+++ very-easy/wordLengths.js
@@ -0,0 +1,6 @@
+// Array of Word Lengths:
+// Create a function that takes an array of words and transforms it into an array of each word's length.
+
+function wordLengths(arr) {
+  return arr.map(word => word.length);
+}

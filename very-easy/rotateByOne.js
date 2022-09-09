38ba49871f6cbc90caec294b1c405dc20e919c19
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-09 01:18:18 -0400

    added solution

---
+++ very-easy/rotateByOne.js
@@ -0,0 +1,7 @@
+// Rotate the Array by One:
+// Given an array, rotate the values clockwise by one (the last value is sent to the first position).
+
+function rotateByOne(arr) {
+  const lastEl = [arr.pop()];
+  return lastEl.concat(arr);
+}

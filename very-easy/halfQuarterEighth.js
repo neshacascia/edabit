6de40283d0fd6da1ecb6b7885a2612aa0e341258
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-11 21:38:08 -0400

    added solution

---
+++ very-easy/halfQuarterEighth.js
@@ -0,0 +1,7 @@
+// Half, Quarter and Eighth:
+// Create a function that takes a number and return an array of three numbers:
+// half of the number, quarter of the number and an eighth of the number.
+
+function halfQuarterEighth(n) {
+  return [n / 2, n / 4, n / 8];
+}

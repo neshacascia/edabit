d5391482e6675dcfb46e8955702a0783760d1ae4
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-11 21:42:47 -0400

    added solution

---
+++ very-easy/length.js
@@ -0,0 +1,6 @@
+// Recreating the String.length Property:
+// Create a function which returns the length of a string, WITHOUT using String.length property.
+
+function length(s) {
+  return s.split('').length;
+}

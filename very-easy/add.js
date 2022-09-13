cb2e3b993552da1cc7649a2b3d0871ef79c0e33c
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-13 02:24:49 -0400

    added solution

---
+++ very-easy/add.js
@@ -0,0 +1,16 @@
+// Arrow Functions:
+// In the Code tab you will find code that is missing a single character in
+// order to pass the tests. However, your goal is to submit a function as
+// minimalist as possible. Use the tips in the tips section below.
+
+// Write five adder functions:
+
+const add2 = x => x + 2;
+
+const add3 = x => x + 3;
+
+const add5 = x => x + 5;
+
+const add7 = x => x + 7;
+
+const add11 = x => x + 11;

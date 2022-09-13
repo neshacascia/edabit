5d3142e44dbe4ab1f8fcdad1944de93db685fdcf
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-13 02:27:40 -0400

    added solution

---
+++ very-easy/yeahNope.js
@@ -0,0 +1,7 @@
+// Using Ternary Operators:
+// Write a function that uses the ternary operator to return "yeah"
+// if bool is true, and "nope" otherwise.
+
+function yeahNope(bool) {
+  return bool ? 'yeah' : 'nope';
+}

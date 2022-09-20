52fb733c7bfe890a1f36e590c2edf89aeec3df76
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-20 04:42:39 -0400

    added solution

---
+++ very-easy/circuitPower.js
@@ -0,0 +1,6 @@
+// Power Calculator:
+// Create a function that takes voltage and current and returns the calculated power.
+
+function circuitPower(voltage, current) {
+  return voltage * current;
+}

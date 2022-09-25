b8ffeda9def0fd4b4ac2f615115ff93c9e872710
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-25 05:02:04 -0400

    added solution

---
+++ very-easy/findPerimeter.js
@@ -0,0 +1,6 @@
+// Find the Perimeter of a Rectangle:
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+function findPerimeter(length, width) {
+  return (length + width) * 2;
+}

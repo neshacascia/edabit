aa1edc903ed8fcd6fbd9d3fb0c854f55e7d8c6bb
Nesha Mervin <nesha.mervin@gmail.com>
2022-09-26 05:05:25 -0400

    added solution

---
+++ very-easy/giveMeSomething.js
@@ -0,0 +1,7 @@
+// Return Something to Me!:
+// Write a function that returns the string "something" joined with a
+// space " " and the given argument a.
+
+function giveMeSomething(a) {
+  return 'something '.concat(a);
+}

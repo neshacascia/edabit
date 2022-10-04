d12d7bea1c99a2ec833b61927bd1f682e169e4cd
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-04 05:32:17 -0400

    added solution

---
+++ very-easy/swap.js
@@ -0,0 +1,7 @@
+// Buggy Code (Part 7):
+// Mubashir wants to swap two given numbers!
+// It is not returning the right values. Can you help him fix it?
+
+function swap(a, b) {
+  return [b, a];
+}

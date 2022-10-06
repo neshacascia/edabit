939907c1ecc30e3098f86a13acc0a34bfe3f0b74
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-06 05:45:48 -0400

    added solution

---
+++ very-easy/and.js
@@ -0,0 +1,7 @@
+// Using the "&&" Operator:
+// JavaScript has a logical operator &&. The && operator takes two boolean values,
+// and returns true if both values are true.
+
+function and(a, b) {
+  return a && b ? true : false;
+}

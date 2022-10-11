0d3f39fd8feb987e69f157790ecba7892a46c8a5
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-10 22:59:53 -0400

    added solution

---
+++ very-easy/checkEquality.js
@@ -0,0 +1,14 @@
+// Equality Check:
+// In this challenge, you must verify the equality of two different values given
+// the parameters a and b.
+
+// Both the value and type of the parameters need to be equal. The possible types
+// of the given parameters are: Numbers, Strings, Booleans (false or true), Special
+// values: undefined, null and NaN
+
+// Implement a function that returns true if the parameters are equal, and false if
+// they are not.
+
+function checkEquality(a, b) {
+  return a === b ? true : false;
+}

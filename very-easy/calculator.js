adf534c24b16f84142f13957c13cabb95116477d
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-17 23:34:52 -0400

    added solution

---
+++ very-easy/calculator.js
@@ -0,0 +1,7 @@
+// Miserable Parody of a Calculator:
+// Create a function that will handle simple math expressions. The input is an
+// expression in the form of a string.
+
+function calculator(str) {
+  return eval(str);
+}

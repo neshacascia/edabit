9b48688f3e25bd1de8a397034a9d94d621e1155d
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-20 23:47:26 -0400

    refactored solution

--- very-easy/makesTen.js
+++ very-easy/makesTen.js
@@ -3,5 +3,10 @@
 // Return true if one of them is 10 or if their sum is 10.
 
 function makesTen(a, b) {
-  return a + b === 10 || a == 10 || b === 10 ? true : false;
+  return a + b === 10 || a == 10 || b === 10;
+}
+
+// Refactored solution:
+function makesTen(a, b) {
+  return [a, b, a + b].includes(10);
 }

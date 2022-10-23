516ef582ddb3a4736d1abc486d3e86e6dbd5d8f4
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-22 23:56:40 -0400

    added solution

---
+++ very-easy/maxNum.js
@@ -0,0 +1,11 @@
+// Buggy Code (Part 2):
+// Fix the code in the code tab to pass this challenge (only syntax errors).
+// Look at the examples below to get an idea of what the function should do.
+
+function maxNum(n1, n2) {
+  if (n1 > n2) {
+    return n1;
+  } else {
+    return n2;
+  }
+}

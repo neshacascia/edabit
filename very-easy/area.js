f1c6a7619d4d374e7036e95bb0cd57959b490b6d
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-24 00:27:09 -0400

    added solution

---
+++ very-easy/area.js
@@ -0,0 +1,7 @@
+// Area of a Rectangle:
+// Create a function that calculates the area of a rectangle. If the arguments
+// are invalid, your function must return -1.
+
+function area(h, w) {
+  return h > 0 && w > 0 ? h * w : -1;
+}

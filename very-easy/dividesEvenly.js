19594040cb779acf36875a23efb520372b6db939
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-25 00:14:48 -0400

    added solution

---
+++ very-easy/dividesEvenly.js
@@ -0,0 +1,7 @@
+// Divides Evenly:
+// Given two integers, a and b, return true if a can be divided evenly by b.
+// Return false otherwise.
+
+function dividesEvenly(a, b) {
+  return a % b === 0;
+}

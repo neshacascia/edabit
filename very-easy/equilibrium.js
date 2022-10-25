f16ce9890bb294794c331a7bc81f0cd93a7e50c6
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-25 02:39:28 -0400

    added solution

---
+++ very-easy/equilibrium.js
@@ -0,0 +1,8 @@
+// Minimal IV: if-else if-else Inferno:
+// Write a function that returns the boolean true if the given number is zero,
+// the string "positive" if the number is greater than zero or the string "negative"
+// if it's smaller than zero.
+
+function equilibrium(x) {
+  return x > 0 ? 'positive' : x < 0 ? 'negative' : true;
+}

74e986e05845f68ebaff8570843724a9de976639
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-29 00:44:58 -0400

    added solution

---
+++ very-easy/concatName.js
@@ -0,0 +1,7 @@
+// Concatenate First and Last Name into One String:
+// Given two strings, firstName and lastName, return a single string in the
+// format "last, first".
+
+function concatName(firstName, lastName) {
+  return `${lastName}, ${firstName}`;
+}

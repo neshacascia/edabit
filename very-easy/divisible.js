6310f3841396eb00f1d81b89f116c0fde67abf63
Nesha Mervin <nesha.mervin@gmail.com>
2022-10-31 00:08:20 -0400

    added solution

---
+++ very-easy/divisible.js
@@ -0,0 +1,7 @@
+// Multiple of 100:
+// Create a function that takes an integer and returns true if it's divisible by
+// 100, otherwise return false.
+
+function divisible(num) {
+  return num % 100 == 0;
+}

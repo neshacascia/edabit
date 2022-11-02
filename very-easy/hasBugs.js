fcfe94dae21c7f14a8e38a45effa4d458aa8f76f
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-02 00:59:26 -0400

    added solution

---
+++ very-easy/hasBugs.js
@@ -0,0 +1,7 @@
+// Buggy Code:
+// The challenge is to try and fix this buggy code, given the inputs true and false.
+// See the examples below for the expected output.
+
+function hasBugs(buggy_code) {
+  return buggy_code ? 'sad days' : "it's a good day";
+}

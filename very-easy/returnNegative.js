f170281c96ac9d6cf849dc7d36539007e39cc868
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-12 00:19:43 -0500

    added solution

---
+++ very-easy/returnNegative.js
@@ -0,0 +1,7 @@
+// Return Negative:
+// Create a function that takes a number as an argument and returns negative of that
+//number. Return negative numbers without any change.
+
+function returnNegative(n) {
+  return n >= 0 ? -n : n;
+}

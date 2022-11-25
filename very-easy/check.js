3de3b2b8f7e3b042d89bf2753ac2c3f0823f7a55
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-24 22:46:51 -0500

    added solution. 24/11/22

---
+++ very-easy/check.js
@@ -0,0 +1,6 @@
+/* Check if an Array Contains a Given Number:
+Write a function to check if an array contains a particular number. */
+
+function check(arr, el) {
+  return arr.includes(el);
+}
---
+++ very-easy/valueAt.js
@@ -0,0 +1,6 @@
+/* Array Indexing:
+Given an index and an array, return the value of the array with the given index. */
+
+function valueAt(arr, i) {
+  return arr[Math.floor(i)];
+}

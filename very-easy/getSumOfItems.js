b6d31df44425e43874b630deb8eeb200f725a494
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-26 08:51:11 -0500

    added solution

---
+++ very-easy/getSumOfItems.js
@@ -0,0 +1,6 @@
+// Get the Sum of All Array Elements:
+// Create a function that takes an array and returns the sum of all numbers in the array.
+
+function getSumOfItems(arr) {
+  return arr.reduce((prev, current) => prev + current);
+}

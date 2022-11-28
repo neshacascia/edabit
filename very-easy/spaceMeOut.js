523cc05a78c56c2cc03a07b06a06d06b813346f0
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-28 08:42:29 -0500

    added solution

---
+++ very-easy/spaceMeOut.js
@@ -0,0 +1,6 @@
+// Spaces Between Each Character:
+// Create a function that takes a string and returns a string with spaces in between all of the characters.
+
+function spaceMeOut(str) {
+  return str.split('').join(' ');
+}

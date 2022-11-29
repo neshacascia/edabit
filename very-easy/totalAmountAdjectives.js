82e14fa1d265072f2ae9c0af1ac0ee7b30f297f1
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-29 02:24:39 -0500

    added solution

---
+++ very-easy/totalAmountAdjectives.js
@@ -0,0 +1,7 @@
+// Burglary Series (14): Adjectives Total:
+// You call your spouse in anger and a "little" argument takes place. Count the total amount of
+// insults used. Given an object of insults, return the total amount of insults used.
+
+function totalAmountAdjectives(obj) {
+  return Object.keys(obj).length;
+}

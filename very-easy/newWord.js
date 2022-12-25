8bdf7ed09a92b8c8d1ebfa78d9d85c1f59dd996a
Nesha Mervin <nesha.mervin@gmail.com>
2022-12-25 00:15:51 -0500

    added solution

---
+++ very-easy/newWord.js
@@ -0,0 +1,6 @@
+// Word without First Character:
+// Create a function that takes a word and returns the new word without including the first character.
+
+function newWord(str) {
+  return str.slice(1);
+}

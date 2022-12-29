adc3e00456b23d04ec3ab5da81d5a3158cf41238
Nesha Mervin <nesha.mervin@gmail.com>
2022-12-29 00:25:40 -0500

    added solution

---
+++ very-easy/isEvenOrOdd.js
@@ -0,0 +1,7 @@
+// Is the Number Even or Odd?:
+// Create a function that takes a number as an argument and returns "even" for even numbers
+// and "odd" for odd numbers.
+
+function isEvenOrOdd(num) {
+  return num % 2 === 0 ? 'even' : 'odd';
+}

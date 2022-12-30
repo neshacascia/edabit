2f14d076d21d129bba0ba5fa095683167e34a926
Nesha Mervin <nesha.mervin@gmail.com>
2022-12-30 00:34:38 -0500

    added solution

---
+++ very-easy/isOdd.js
@@ -0,0 +1,8 @@
+// Fix the Error: Check Whether a Given Number Is Odd:
+// Éowyn has written the function isOdd() to check if a given number is odd or not.
+// Unfortunately, the function does not return the correct result for all the inputs.
+// Help her fix the error.
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}

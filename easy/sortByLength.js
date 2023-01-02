74854c68c34ec8d8873d2a676ad48e921132c009
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-01 22:46:54 -0500

    added solution

---
+++ easy/sortByLength.js
@@ -0,0 +1,6 @@
+// Sort by String Length:
+// Create a function that returns an array of strings sorted by length in ascending order.
+
+function sortByLength(arr) {
+  return arr.sort((a, b) => a.length - b.length);
+}

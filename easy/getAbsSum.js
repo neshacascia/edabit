b009e46bcead22ce0ccba241f0598c1010ca800f
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-04 13:39:53 -0500

    Revert "added solution"
    This reverts commit 563dac6aeba9673015ef9ceb70e8a157d2dc57fc.

--- easy/getAbsSum.js
+++
@@ -1,7 +0,0 @@
-// Absolute Sum:
-// Take an array of integers (positive or negative or both) and return the sum
-// of the absolute value of each element.
-
-function getAbsSum(arr) {
-  return arr.reduce((prev, curr) => prev + Math.abs(curr), 0);
-}

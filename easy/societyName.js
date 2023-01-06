f20ffa1b6986a812bd221323aba1bfcbb9ea6ce9
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-05 20:48:18 -0500

    added solution

---
+++ easy/societyName.js
@@ -0,0 +1,12 @@
+// Secret Society:
+// A group of friends have decided to start a secret society. The name will be the first
+// letter of each of their names, sorted in alphabetical order.
+
+// Create a function that takes in an array of names and returns the name of the secret society.
+
+function societyName(friends) {
+  return friends
+    .sort()
+    .map(friend => friend.toString().slice(0, 1))
+    .join('');
+}

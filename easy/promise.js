51ddeee5cfff495f68c9e8c25ecfc371dab6c766
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-01 15:05:03 -0500

    added solution

---
+++ easy/promise.js
@@ -0,0 +1,10 @@
+// Promises III: Native Promise, Introducing the Executor:
+// Create a simple promise and pass the resolve function a string value of your choice.
+// Use the setTimeout function as your asynchronous operation. Your setTimeout() function
+// should not exceed 1000ms. Store the promise inside a variable named promise.
+
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hello World');
+  }, 1000);
+});

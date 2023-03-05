8e99617efe69bd679656c7369e2d8715ebf2c5c9
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-05 04:38:02 -0500

    added solution

---
+++ very-easy/stringNumberConversions.js
@@ -0,0 +1,13 @@
+// You need to create two functions to substitute toString() and parseInt(); A function called
+// intToString() that converts integers into strings and a function called stringToInt() that
+// converts strings into integers.
+
+// You will get bonus points if you manage to solve this without using toString, parseInt or parseFloat.
+
+function intToString(num) {
+  return `${num}`;
+}
+
+function stringToInt(num) {
+  return Number(num);
+}

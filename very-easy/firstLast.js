31be7d764ca976818727eba32075de1f0e11623f
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-06 05:11:05 -0500

    added solution

---
+++ very-easy/firstLast.js
@@ -0,0 +1,6 @@
+// Concatenating First and Last Character of a String
+// Create a function that takes a string and returns the concatenated first and last character.
+
+function firstLast(name) {
+  return name.slice(0, 1) + name.slice(-1);
+}

3b98727c060e201a9be5584e788eb7bab689bbd3
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-06 04:45:13 -0500

    added solution

---
+++ very-easy/word.js
@@ -0,0 +1,31 @@
+// Word Numbers!
+// Create a function that returns a number, based on the string provided. Here is a list of all digits:
+
+// String	Number
+// "one"	1
+// "two"	2
+// "three"	3
+// "four"	4
+// "five"	5
+// "six"	6
+// "seven"	7
+// "eight"	8
+// "nine"	9
+// "zero"	0
+
+function word(s) {
+  const words = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  return words.indexOf(s);
+}

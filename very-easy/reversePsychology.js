697d8b77c4c123b1838c029a08bdc7b3dbebcafb
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-06 05:14:13 -0500

    added solution

---
+++ very-easy/reversePsychology.js
@@ -0,0 +1,8 @@
+// Reverse Psychology
+// For this challenge, you will NOT be given a string. Your task isn't to add "Do not" before
+// the given string. If there is no given string, you will not return "Do not do anything."
+// Do not check the examples to know how to do this challenge.
+
+function reversePsychology(s) {
+  return s ? `Do not ${s}.` : 'Do not do anything.';
+}

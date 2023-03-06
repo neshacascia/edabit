a3097822f2d95799b474d87b72796ef40c6eb99d
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-06 04:49:47 -0500

    added solution

---
+++ very-easy/repeatString.js
@@ -0,0 +1,13 @@
+// Repeat String
+// Create a function that takes a string txt and a number n and returns the repeated string
+// n number of times.
+
+// If given argument txt is not a string, return Not A String !!
+
+function repeatString(txt, n) {
+  if (typeof txt !== 'string') {
+    return 'Not A String !!';
+  } else {
+    return txt.repeat(n);
+  }
+}

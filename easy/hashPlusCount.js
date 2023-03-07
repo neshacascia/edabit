a16183b044854aad6cdad75f76e539af02ee1711
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-07 13:02:40 -0500

    added solution

---
+++ easy/hashPlusCount.js
@@ -0,0 +1,17 @@
+// Hashes and Pluses
+// Create a function that returns the number of hashes and pluses in a string.
+// Return in the order of [hashes, pluses].
+
+function hashPlusCount(str) {
+  let hashCount = 0;
+  let plusCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '#') {
+      hashCount++;
+    } else {
+      plusCount++;
+    }
+  }
+  return [hashCount, plusCount];
+}

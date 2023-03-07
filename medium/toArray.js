ac822df72869183965ad25d1d7f7a9debbb612b9
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-07 13:07:47 -0500

    added solution

---
+++ medium/toArray.js
@@ -0,0 +1,7 @@
+// Converting Objects to Arrays
+// Write a function that converts an object into an array, where each element represents a
+// key-value pair in the form of an array.
+
+function toArray(obj) {
+  return Object.entries(obj);
+}

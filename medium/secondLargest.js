00988a46f16de8679afaf8fd23dd366efc5fffe2
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-28 22:05:45 -0400

    refactored solution

--- medium/secondLargest.js
+++ medium/secondLargest.js
@@ -1,4 +1,12 @@
+// Find the Second Largest Number:
+// Create a function that takes an array of numbers and returns the second largest number.
+
 function secondLargest(arr) {
   const sortedArr = arr.sort((a, b) => a - b);
   return sortedArr[sortedArr.length - 2];
+}
+
+//Refactored solution:
+function secondLargest(arr) {
+  return arr.sort((a, b) => a - b)[1];
 }

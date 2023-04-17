cef6326144fada50ed3e18acc30d6fe5e7a152d5
Nesha Mervin <nesha.mervin@gmail.com>
2023-04-16 23:34:31 -0400

    added solution

---
+++ medium/removeDups.js
@@ -0,0 +1,15 @@
+// Remove Duplicates from an Array:
+// Create a function that takes an array of items, removes all duplicate items
+// and returns a new array in the same sequential order as the old array (minus
+// duplicates).
+
+function removeDups(arr) {
+  const newArr = [];
+  arr.filter(elem => {
+    if (!newArr.includes(elem)) {
+      newArr.push(elem);
+    }
+  });
+
+  return newArr;
+}

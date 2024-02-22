84bccd135884ab8500dd41055292671f9d8ed541
Nesha Mervin <nesha.mervin@gmail.com>
2024-02-22 07:49:55 -0500

    added solution

---
+++ python/very-easy/addition.py
@@ -0,0 +1,8 @@
+# Return the Sum of Two Numbers
+# Create a function that takes two numbers as arguments and returns their sum.
+
+# URL:
+# https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
+
+def addition(a, b):
+	return a + b

839a7f2e174b07616ce0dccfd695dd4d6b423ee6
Nesha Mervin <nesha.mervin@gmail.com>
2024-02-23 07:52:24 -0500

    added solution

---
+++ python/very-easy/addition-2.py
@@ -0,0 +1,8 @@
+# Return the Next Number from the Integer Passed
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+# URL:
+# https://edabit.com/challenge/KjCS7occ9hfu5snpb
+
+def addition(num):
+    return num + 1

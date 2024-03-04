4e50f83c73216452bef93aac5acca57ff11afb4e
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-04 07:54:35 -0500

    added solution

---
+++ python/very-easy/convert.py
@@ -0,0 +1,8 @@
+# Convert Minutes into Seconds
+# Write a function that takes an integer minutes and converts it to seconds.
+
+# URL:
+# https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+
+def convert(minutes):
+	return minutes * 60

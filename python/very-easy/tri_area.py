daff1fef8679996ba5ce9780eb23a7dd4f4bdce8
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-05 08:03:43 -0500

    added solution

---
+++ python/very-easy/tri_area.py
@@ -0,0 +1,8 @@
+# Area of a Triangle
+# Write a function that takes the base and height of a triangle and return its area.
+
+# URL:
+# https://edabit.com/challenge/aWLTzrRsrw7RakYrN
+
+def tri_area(base, height):
+	return (base * height) / 2

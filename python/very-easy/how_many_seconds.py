b5eae2ed02ae7353fe669ad0c0bb996b80e09ce0
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-08 08:05:52 -0500

    added solution

---
+++ python/very-easy/how_many_seconds.py
@@ -0,0 +1,8 @@
+# Convert Hours into Seconds
+# Write a function that converts hours into seconds.
+
+# URL:
+# https://edabit.com/challenge/nyeNvKWdDFKRAk4Da
+
+def how_many_seconds(hours):
+	return hours * 60 * 60

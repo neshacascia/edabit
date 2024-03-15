18a14c349d82ca9eeffbac716aafa131c8b4768e
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-15 09:07:52 -0400

    added solution

---
+++ python/very-easy/remainder.py
@@ -0,0 +1,10 @@
+# Return the Remainder from Two Numbers
+# There is a single operator in Python, capable of providing the remainder of a division operation. 
+# Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. 
+# Return that value.
+
+# URL:
+# https://edabit.com/challenge/KWoj7kWiHRqJtG6S2
+
+def remainder(x, y):
+	return x % y

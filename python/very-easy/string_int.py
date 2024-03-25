95fe2ee8c921caafa12e92644e11bbe2fa33c5b4
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-25 09:11:19 -0400

    added solution

---
+++ python/very-easy/string_int.py
@@ -0,0 +1,8 @@
+# Return a String as an Integer
+# Create a function that takes a string and returns it as an integer.
+
+# URL:
+# https://edabit.com/challenge/GPmoRCZKkyNtoJMcN
+
+def string_int(txt):
+	return int(txt)

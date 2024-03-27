34eb72b58777f2560134e70c80fa94e9ce2011f4
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-27 09:13:59 -0400

    added solution

---
+++ python/very-easy/calc_age.py
@@ -0,0 +1,8 @@
+# Convert Age to Days
+# Create a function that takes the age in years and returns the age in days.
+
+# URL:
+# https://edabit.com/challenge/xbZR26rHMNo32yz35
+
+def calc_age(age):
+	return age * 365

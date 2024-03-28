b9fd3933114586d912fa3d60fbf6e0e2b0148094
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-28 09:15:56 -0400

    added solution

---
+++ python/very-easy/find_perimeter.py
@@ -0,0 +1,8 @@
+# Find the Perimeter of a Rectangle
+# Create a function that takes length and width and finds the perimeter of a rectangle.
+
+# URL:
+# https://edabit.com/challenge/Yx2a9B57vXRuPevGh
+
+def find_perimeter(length, width):
+	return (length + width) * 2

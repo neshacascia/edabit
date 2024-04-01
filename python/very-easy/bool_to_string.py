a17f8799cd430f803a236d0a5957b4d1c6bf2110
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-01 09:17:27 -0400

    added solution

---
+++ python/very-easy/bool_to_string.py
@@ -0,0 +1,8 @@
+# Boolean to String Conversion
+# Create a function that takes a boolean variable flag and returns it as a string.
+
+# URL:
+# https://edabit.com/challenge/QQp2o22huzBCkHesy
+
+def bool_to_string(flag):
+	return str(flag)

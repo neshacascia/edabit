5a2984075b33281a3219f562e8377970b835cd09
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-04 09:19:04 -0400

    added solution

---
+++ python/very-easy/get_first_value.py
@@ -0,0 +1,8 @@
+# Return the First Element in a List
+# Create a function that takes a list containing only numbers and return the first element.
+
+# URL:
+# https://edabit.com/challenge/hEQ3rBrKrztQK8qAd
+
+def get_first_value(number_list):
+	return number_list[0]

40c4aa066396945cf9785265b4d6002c18ab5727
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-09 09:21:47 -0400

    added solution

---
+++ python/very-easy/circuit_power.py
@@ -0,0 +1,8 @@
+# Power Calculator
+# Create a function that takes voltage and current and returns the calculated power.
+
+# URL:
+# https://edabit.com/challenge/v5gc8FQkDEepkqpfp
+
+def circuit_power(voltage, current):
+	return voltage * current

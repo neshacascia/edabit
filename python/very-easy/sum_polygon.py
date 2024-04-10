d1ff4d13db6b0ec80cb99178042aaa7453af8124
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-10 10:09:27 -0400

    added solution

---
+++ python/very-easy/sum_polygon.py
@@ -0,0 +1,8 @@
+# Sum of Polygon Angles
+# Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+# URL:
+# https://edabit.com/challenge/mDzheHpwtqyXePEBE
+
+def sum_polygon(n):
+	return (n - 2) * 180

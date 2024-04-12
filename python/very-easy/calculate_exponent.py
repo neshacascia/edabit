76e2363904a368fd4e675fa8297817facd02f8bd
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-12 10:13:21 -0400

    added solution

---
+++ python/very-easy/calculate_exponent.py
@@ -0,0 +1,8 @@
+# To the Power of _____
+# Create a function that takes a base number and an exponent number and returns the calculation.
+
+# URL:
+# https://edabit.com/challenge/xWSjvoH7mEkSnqS7H
+
+def calculate_exponent(num, exp):
+	print(num ** exp)

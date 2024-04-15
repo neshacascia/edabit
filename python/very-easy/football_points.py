116044f83e698306cc31032e38a6695c21087e95
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-15 10:16:46 -0400

    added solution

---
+++ python/very-easy/football_points.py
@@ -0,0 +1,13 @@
+# Football Points
+# Create a function that takes the number of wins, draws and losses and calculates the number of points a football 
+# team has obtained so far.
+# - wins get 3 points
+# - draws get 1 point
+# - losses get 0 points
+
+# URL:
+# https://edabit.com/challenge/gwqqc5p3oiFXRJAQm
+
+def football_points(wins, draws, losses):
+    print(wins * 3 + draws * 1)
+

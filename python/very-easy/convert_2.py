015e3469e8d21531d84220edb8b107a749f3829b
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-17 10:22:45 -0400

    added solution

---
+++ python/very-easy/convert_2.py
@@ -0,0 +1,8 @@
+# Convert Hours and Minutes into Seconds
+# Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+
+# URL:
+# https://edabit.com/challenge/PjcKZRx8YE5KzRN63
+
+def convert(hours, minutes):
+	print(hours * 60 * 60 + minutes * 60)

dc4ed69f03b532cea78d7a06f4e6d95df49ba69a
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-29 10:36:03 -0400

    added solution

---
+++ python/very-easy/is_equal.py
@@ -0,0 +1,9 @@
+# Check if objOne Is Equal to objTwo
+# Create a function that checks to see if two object arguments are equal to one another. 
+# Return True if the objects are equal, otherwise, return False.
+
+# URL:
+# https://edabit.com/challenge/QJiqdu9QGMmAgK5Fi
+
+def is_equal(obj_one, obj_two):
+	return obj_one.values() == obj_two.values()

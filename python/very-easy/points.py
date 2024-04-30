124c296e4312a7177f2725bd03b0062c6a015621
Nesha Mervin <nesha.mervin@gmail.com>
2024-04-30 10:37:43 -0400

    added solution

---
+++ python/very-easy/points.py
@@ -0,0 +1,9 @@
+# Basketball Points
+# You are counting points for a basketball game, given the amount of 3-pointers scored and 2-pointers scored, 
+# find the final points for the team and return that value ([2 -pointers scored, 3-pointers scored]).
+
+# URL:
+# https://edabit.com/challenge/KWnJrMzK9CumnfxTF
+
+def points(twopointers, threepointers):
+	return twopointers * 2 + threepointers * 3

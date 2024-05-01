6fe9bbb3bf12fabb77f900607513cee5e02104bb
Nesha Mervin <nesha.mervin@gmail.com>
2024-05-01 10:39:03 -0400

    added solution

---
+++ python/very-easy/is_same_num.py
@@ -0,0 +1,9 @@
+# Are the Numbers Equal?
+# Create a function that returns True when num1 is equal to num2; otherwise return False.
+
+# URL:
+# https://edabit.com/challenge/yfooETHj3sHoHTJsv
+
+def is_same_num(num1, num2):
+	print( num1 == num2 )
+

3a7fa9a89a163693cdafd571492b460340da2ab0
Nesha Mervin <nesha.mervin@gmail.com>
2024-05-02 10:42:44 -0400

    added solution

---
+++ python/very-easy/less_than_or_equal_to_zero.py
@@ -0,0 +1,8 @@
+# Is the Number Less than or Equal to Zero?
+# Create a function that takes a number as its only argument and returns True if it's less than or equal to zero, otherwise return False.
+
+# URL:
+# https://edabit.com/challenge/Rx2pkSA9dCmtwS8xt
+
+def less_than_or_equal_to_zero(num):
+	return True if num <= 0 else False 

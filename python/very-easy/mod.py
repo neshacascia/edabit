efa5b8f5be3314672c35769a67c2b56eb5893c82
Nesha Mervin <nesha.mervin@gmail.com>
2024-05-06 10:46:51 -0400

    added solution

---
+++ python/very-easy/mod.py
@@ -0,0 +1,8 @@
+# Proper Modulo Operator
+# Create a function which returns the Modulo of the two given numbers.
+
+# URL:
+# https://edabit.com/challenge/D2Hvq6NZchp7Q6ftR
+
+def mod(m, n):
+	return m % n

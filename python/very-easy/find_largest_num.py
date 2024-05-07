00b31e33bbfcb66b3597192c9313596b79f7e5c6
Nesha Mervin <nesha.mervin@gmail.com>
2024-05-07 10:52:03 -0400

    added solution

---
+++ python/very-easy/find_largest_num.py
@@ -0,0 +1,10 @@
+# Find the Largest Number in a List
+# Create a function that takes a list of numbers. Return the largest number in the list.
+
+# URL:
+# https://edabit.com/challenge/A7hyDnb72prWryeuY
+
+def findLargestNum(nums):
+    sorted_list = sorted(nums)
+    return sorted_list[-1]
+

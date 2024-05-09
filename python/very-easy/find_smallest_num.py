dce1c2e59d1fa941e5d9cecc3f0aee3cd3cf4d0d
Nesha Mervin <nesha.mervin@gmail.com>
2024-05-09 10:57:18 -0400

    added solution

---
+++ python/very-easy/find_smallest_num.py
@@ -0,0 +1,9 @@
+# Find the Smallest Number in a List
+# Create a function that takes a list of numbers and returns the smallest number in the list.
+
+# URL:
+# https://edabit.com/challenge/ecSZ5kDBwCD3ctjE6
+
+def find_smallest_num(lst):
+    sorted_list = sorted(lst)
+    return sorted_list[0])

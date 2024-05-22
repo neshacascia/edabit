1985358c77b3b323e6f864f4314bbc4c8d219660
Nesha Mervin <nesha.mervin@gmail.com>
2024-05-22 10:28:52 -0400

    added solution

---
+++ python/very-easy/total_amount_adjectives.py
@@ -0,0 +1,9 @@
+# Burglary Series (14): Adjectives Total
+# You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. 
+# Given a dictionary of insults, return the total amount of insults used.
+
+# URL:
+# https://edabit.com/challenge/jKAjLk5epb8XDzTwC
+
+def total_amount_adjectives(dct):
+	print(len(dct.keys()))

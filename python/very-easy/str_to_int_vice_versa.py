26764cf79ea4145fd9c3dd439f4b5d0b2b88f929
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-09 10:31:47 -0400

    added solution

---
+++ python/very-easy/str_to_int_vice_versa.py
@@ -0,0 +1,15 @@
+# String to Integer and Vice Versa
+# Write two functions:
+# to_int() : A function to convert a string to an integer.
+# to_str() : A function to convert an integer to a string.
+
+# URL:
+# https://edabit.com/challenge/WKJwo2xDNjKxwtGoH
+
+def to_int(txt):
+	return int(txt)
+
+def to_str(num):
+	return str(num)
+
+

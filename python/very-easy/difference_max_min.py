9ab1bb709b9524ae7dd8d2983dbce1c2a290634f
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-02 11:33:42 -0400

    added solution

---
+++ python/very-easy/difference_max_min.py
@@ -0,0 +1,12 @@
+# Difference of Max and Min Numbers in List
+# Create a function that takes a list and returns the difference between the biggest and smallest numbers.
+
+# URL:
+# https://edabit.com/challenge/XsJLwhAddzbxdQqr4
+
+def difference_max_min(lst):
+	max_num = max(lst)
+	min_num = min(lst)
+	
+	return max_num - min_num
+

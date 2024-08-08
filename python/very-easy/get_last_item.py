45f69e05aa9256886f78060d8640084fcb54ecb7
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-08 12:25:13 -0400

    added solution

---
+++ python/very-easy/get_last_item.py
@@ -0,0 +1,10 @@
+# Return the Last Element in a List
+# Create a function that accepts a list and returns the last item in the list.
+# The list can be either homogeneous or heterogeneous.
+
+# URL:
+# https://edabit.com/challenge/uPtuNNTuASzPZMQrW
+
+
+def get_last_item(lst):
+    return lst[-1]

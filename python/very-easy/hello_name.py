ca9d11e24a41275905947a40df40a82fcc1aaade
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-09 12:26:33 -0400

    added solution

---
+++ python/very-easy/hello_name.py
@@ -0,0 +1,9 @@
+# Name Greeting!
+# Create a function that takes a name and returns a greeting in the form of a string.
+
+# URL:
+# https://edabit.com/challenge/coRuMC4Ykksti8Z47
+
+
+def hello_name(name):
+    return "Hello " + name

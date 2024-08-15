63be233c0ee08afc768fa61d8aa9d46b7a4e168c
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-15 12:53:25 -0400

    added solution

---
+++ python/very-easy/makes_10.py
@@ -0,0 +1,16 @@
+# Two Makes Ten
+# Create a function that takes two arguments. Both arguments are integers, a and b.
+# Return True if one of them is 10 or if their sum is 10.
+
+
+# URL:
+# https://edabit.com/challenge/HuWQaCpFR7iTeCvTm
+
+
+def makes_10(a, b):
+    if a == 10 or b == 10:
+        return True
+    elif a + b == 10:
+        return True
+    else:
+        return False

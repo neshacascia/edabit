2a9fa776b9ed8f8a52f2922a3b69a6236ba42936
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-19 13:17:27 -0400

    added solution

---
+++ python/very-easy/get_sum_of_elements.py
@@ -0,0 +1,14 @@
+# Get the Sum of All List Elements
+# Create a function that takes a list and returns the sum of all numbers in the list.
+
+# URL:
+# https://edabit.com/challenge/foFKdr68vSENQ9AYB
+
+
+def get_sum_of_elements(lst):
+    sum = 0
+
+    for num in lst:
+        sum += num
+
+    return sum

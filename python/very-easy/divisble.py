5ac50ab7fd6739f86476bfee4ca32393d950fca7
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-20 13:21:15 -0400

    added solution

---
+++ python/very-easy/divisble.py
@@ -0,0 +1,9 @@
+# Multiple of 100
+# Create a function that takes an integer and returns True if it's divisible by 100, otherwise return False.
+
+# URL:
+# https://edabit.com/challenge/NebFhjXTn8NEbhYXY
+
+
+def divisible(num):
+    return num % 100 == 0

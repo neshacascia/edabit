12cc9a6928e528ff14c85747277011c30ba90fdb
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-21 13:24:24 -0400

    added solution

---
+++ python/very-easy/comp.py
@@ -0,0 +1,10 @@
+# Compare Strings by Count of Characters
+# Create a function that takes two strings as arguments and return either True or False depending on whether the total
+# number of characters in the first string is equal to the total number of characters in the second string.
+
+# URL:
+# https://edabit.com/challenge/C3N2JEfFQoh4cqQ98
+
+
+def comp(txt1, txt2):
+    return len(txt1) == len(txt2)

db302dc2362e479d3c4ea9cc40bbebe3aa6ea337
Nesha Mervin <nesha.mervin@gmail.com>
2024-09-02 15:24:25 -0400

    added solution

---
+++ python/very-easy/check.py
@@ -0,0 +1,9 @@
+# Check if a List Contains a Given Number
+# Write a function to check if a list contains a particular number.
+
+# URL:
+# https://edabit.com/challenge/ZGezQDXsturZGpQcS
+
+
+def check(lst, el):
+    return True if el in lst else False
